e7022d3648b5a7c4257fce1e5dc94eec24935e6a
Mario Lazzari <mario.lazzari@gmail.com>
2026-04-28 09:19:16 +0200

    Done lesson 14

--- libs/go-httpclient/example.go
+++ libs/go-httpclient/example.go
@@ -12,14 +12,38 @@ var githubClient = gohttp.New()
 
 func main() {
 	getUrls()
+	createUser(User{
+		FirstName: "Mario",
+		LastName:  "Lazzari",
+	})
+}
+
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
 
 func getUrls() {
-
 	headers := make(http.Header)
-	headers.Set("Authorization", "Bearer 123abc")
+	headers.Set("Authorization", "")
 
 	response, err := githubClient.Get("https://api.github.com", headers)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(response.StatusCode)
+
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+
+}
+
+func createUser(user User) {
+
+	headers := make(http.Header)
+	headers.Set("Authorization", "")
+
+	response, err := githubClient.Post("https://api.github.com", headers, user)
 	if err != nil {
 		log.Fatal(err)
 	}
